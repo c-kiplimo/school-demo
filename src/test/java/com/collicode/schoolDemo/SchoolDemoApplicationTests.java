10ae2e7ced4a43c0437b95f76e65bd289672e946
LimoCollins <90177693+LimoCollins@users.noreply.github.com>
2022-07-21 11:46:35 +0300

    Initial commit

---
+++ src/test/java/com/collicode/schoolDemo/SchoolDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collicode.schoolDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
