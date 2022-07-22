58b66f9d2ee467a58e41a7aa1b67d8e66a4ee359
LimoCollins <90177693+LimoCollins@users.noreply.github.com>
2022-07-22 16:07:41 +0300

    some changes on student repository

--- src/main/java/com/collicode/schoolDemo/repository/StudentRepository.java
+++ src/main/java/com/collicode/schoolDemo/repository/StudentRepository.java
@@ -2,6 +2,6 @@ package com.collicode.schoolDemo.repository;
 
 import com.collicode.schoolDemo.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//extends
 public interface StudentRepository extends JpaRepository<Student,Long> {
 }
