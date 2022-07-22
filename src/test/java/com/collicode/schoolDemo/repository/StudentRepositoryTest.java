ec3d43094448aaf2663be0de39a097ac01500612
LimoCollins <90177693+LimoCollins@users.noreply.github.com>
2022-07-22 16:12:15 +0300

    some changes on student repository tests

--- src/test/java/com/collicode/schoolDemo/repository/StudentRepositoryTest.java
+++ src/test/java/com/collicode/schoolDemo/repository/StudentRepositoryTest.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class StudentRepositoryTest {
     @Autowired
