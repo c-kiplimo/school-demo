784975f904406c77dc90f480f66a2a7721905e49
LimoCollins <90177693+LimoCollins@users.noreply.github.com>
2022-07-22 17:58:59 +0300

    some changes on student repository tests

--- src/test/java/com/collicode/schoolDemo/repository/StudentRepositoryTest.java
+++ src/test/java/com/collicode/schoolDemo/repository/StudentRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.collicode.schoolDemo.repository;
 
+import com.collicode.schoolDemo.entity.Guardian;
 import com.collicode.schoolDemo.entity.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +16,53 @@ class StudentRepositoryTest {
     private StudentRepository studentRepository;
     @Test
     public  void  saveStudent(){
-        Student student= Student.builder().emailId("limo@gmail.com").firstname("collins").lastName("limo").guardianName("David").guardianEmail("david@gmail.com").guardianMobile("34567895346774").build();
+        Student student= Student.builder().emailId("limo@gmail.com").firstname("collins").lastName("limo").build();
         studentRepository.save(student);
     }
     @Test
+    public  void  printStudentByFirstName(){
+   List<Student> students =studentRepository.findByFirstname("collins");
+        System.out.println("students = "+ students);
+    }
+    @Test
+    public void printGetStudentFirstNameByEmailAddress(){
+        String firstN=studentRepository.getFirstNameByEmailAddress("kevo@gmail.com");
+        System.out.println("firstName = " +firstN);
+    }
+    @Test
+    public  void printgetStudentByEmailAdress(){
+        Student student= studentRepository.getStudentByEmailAddress("limo@gmail.com");
+        System.out.println("students = "+ student);
+    }
+    @Test
+    public  void  printStudentBasedOnGuardianName(){
+        List<Student> students=studentRepository.findByGuardianName("Davide");
+        System.out.println("students =" +students);
+    }
+    @Test
+    public  void  printStudentByFirstNameContaing(){
+        List<Student> students =studentRepository.findByFirstnameContaining("i");
+        System.out.println("students = "+ students);
+    }
+    @Test
+    public void saveStudentWithGuardian(){
+        Guardian guardian=Guardian.builder().email("jer@gmail.com").mobile("345678987654").name("jer").build();
+      Student student=Student.builder().firstname("Kelvin").lastName("Kiplimo").emailId("kevo@gmail.com").guardian(guardian).build();
+      studentRepository.save(student);
+      }
+    @Test
     public  void printAllStudent(){
         List<Student> studentList =studentRepository.findAll();
-        System.out.println("studentList =" + studentList);
+        System.out.println("studentList =" + studentList );
+    }
+    @Test
+    public  void  updateStudentfirstNameByEmailId(){
+        studentRepository.updateStudentfirstNameByEmailId("kiplimo collins","limo@gmail.com");
+    }
+
+    @Test
+   public void getStudentFirstNameByEmailAddress() {
+        Student student= studentRepository.getStudentFirstNameByEmailAddress("kevo@gmail.com");
+        System.out.println("student = "+student);
     }
 }
